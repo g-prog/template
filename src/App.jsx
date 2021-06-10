aaa867e7c8ffee414aefac0d4cbd561d6793a4ec
g-prog <alaobukky2@gmail.com>
2021-06-10 07:53:19 -0700

    fifth layer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Intro from "./components/intro/intro";
 import Expone from "./components/expone/expone";
 import Testone from "./components/testone/testone";
 import Exptwo from "./components/exptwo/exptwo";
+import Testtwo from "./components/testtwo/testtwo";
 
 
 
@@ -21,6 +22,7 @@ function App() {
       <Expone/>
       <Testone/>
       <Exptwo/>
+      <Testtwo/>
 
 
       
